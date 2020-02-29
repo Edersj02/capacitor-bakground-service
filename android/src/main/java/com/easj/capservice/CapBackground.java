536996fafe371a3455a54635a0d8693cf364bc3a
esantacruz <esantacruz@transportationamerica.com>
2020-02-29 12:18:43 -0500

    Feat: chanel notification

--- android/src/main/java/com/easj/capservice/CapBackground.java
+++ android/src/main/java/com/easj/capservice/CapBackground.java
@@ -1,9 +1,13 @@
 package com.easj.capservice;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 
 import com.easj.capservice.services.SignalRService;
@@ -35,14 +39,38 @@ public class CapBackground extends Plugin {
     }
 
     @PluginMethod()
-    public void startBackgroundService() {
+    public void startBackgroundService(PluginCall call) {
         context = this.getContext();
+        createChanelIdNotifications();
         Intent intent = new Intent(context, SignalRService.class);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             activity.startForegroundService(intent);
         } else {
             activity.startService(intent);
+        }
+        JSObject ret = new JSObject();
+        ret.put("value", "Start Service");
+        call.resolve(ret);
+    }
+
+    private void createChanelIdNotifications(){
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        String CHANNEL_ID = "com.easj.capservice";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Notifications App";
+            String description = "Background Service";
+            int importance = NotificationManager.IMPORTANCE_NONE;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            channel.setLightColor(Color.BLUE);
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = this.context.getSystemService(NotificationManager.class);
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
         }
     }
 }
