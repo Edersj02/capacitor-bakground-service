5f309ee17eec9dae498d6e24412c90d0e5503773
esantacruz <esantacruz@transportationamerica.com>
2020-03-02 10:33:33 -0500

    Fix: service

--- android/src/main/java/com/easj/capservice/services/SignalRService.java
+++ android/src/main/java/com/easj/capservice/services/SignalRService.java
@@ -8,6 +8,7 @@ import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
+import android.os.Process;
 import android.util.Log;
 
 import com.getcapacitor.ui.Toast;
@@ -18,35 +19,67 @@ public class SignalRService extends Service {
 
     private static final String SERVICE_NAME = SignalRService.class.getName();
 
-    @Override
-    public void onCreate() {
-        HandlerThread thread = new HandlerThread("ServiceStartArguments",
-                THREAD_PRIORITY_BACKGROUND);
-        Toast.show(this, "Service Start");
+    private Looper serviceLooper;
+    private ServiceHandler serviceHandler;
+
+    // Handler that receives messages from the thread
+    private final class ServiceHandler extends Handler {
+        public ServiceHandler(Looper looper) {
+            super(looper);
+        }
+        @Override
+        public void handleMessage(Message msg) {
+            // Normally we would do some work here, like download a file.
+            // For our sample, we just sleep for 5 seconds.
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                // Restore interrupt status.
+                Thread.currentThread().interrupt();
+            }
+            // Stop the service using the startId, so that we don't stop
+            // the service in the middle of handling another job
+            stopSelf(msg.arg1);
+        }
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        return null;
+    public void onCreate() {
+        // Start up the thread running the service. Note that we create a
+        // separate thread because the service normally runs in the process's
+        // main thread, which we don't want to block. We also make it
+        // background priority so CPU-intensive work doesn't disrupt our UI.
+        HandlerThread thread = new HandlerThread("ServiceStartArguments",
+                Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+
+        // Get the HandlerThread's Looper and use it for our Handler
+        serviceLooper = thread.getLooper();
+        serviceHandler = new ServiceHandler(serviceLooper);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.show(this, "service starting");
+        Log.d(SERVICE_NAME, "Service -----");
+        // For each start request, send a message to start a job and deliver the
+        // start ID so we know which request we're stopping when we finish the job
+        Message msg = serviceHandler.obtainMessage();
+        msg.arg1 = startId;
+        serviceHandler.sendMessage(msg);
 
-        try {
-            if (intent != null) {
-                Log.d(SERVICE_NAME, "This is a Background service for Android");
-            }
-        } catch (Exception ex) {
-            Toast.show(this.getBaseContext(), "Unknown error: " + ex.getMessage());
-        }
-
+        // If we get killed, after returning from here, restart
         return START_STICKY;
     }
 
     @Override
+    public IBinder onBind(Intent intent) {
+        // We don't provide binding, so return null
+        return null;
+    }
+
+    @Override
     public void onDestroy() {
-        Toast.show(this, "Service Done");
+        Toast.show(this, "service done");
     }
 }
