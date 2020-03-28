5be088c9856b3b19ebe4bbb4d2b515754fecc8e3
Eder Santa Cruz <edersj02@gmail.com>
2020-03-28 08:37:10 -0500

    Fix: active

--- android/src/main/java/com/easj/capservice/CapBackground.java
+++ android/src/main/java/com/easj/capservice/CapBackground.java
@@ -107,13 +107,14 @@ public class CapBackground extends Plugin implements GoogleApiClient.ConnectionC
         String token = call.getString("token");
         String url = call.getString("url");
         String socketUrl = call.getString("socketurl");
+        boolean active = call.getString("socketactive").equals("true");
         sessionData.setDriverId(id);
         sessionData.setDriverName(name);
         sessionData.setPin(pin);
         sessionData.setToken(token);
         sessionData.setUrl(url);
         sessionData.setSocketUrl(socketUrl);
-        sessionData.setSocketActive(false);
+        sessionData.setSocketActive(active);
         Log.d(CLASS_NAME, "Token -----" + sessionData.getToken());
         preferences.save(sessionData);
         if (networkStatus()) {
